9eecabd1b553a124924993fa18eaec403ada9ff9
Torkel Ödegaard <torkel@grafana.org>
2017-04-16 11:41:19 +0200

    updated

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var GitHubApi = require("github");
 var env = require('node-env-file');
 var Queue = require('promise-queue');
+var _ = require('lodash');
+var db = require('./db');
+var program = require('commander');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: 'app'});
+var moment = require('moment');
+
+log.info("Starting");
 
 // set promise
 Queue.configure(require('bluebird'));
@@ -15,7 +23,8 @@ var github = new GitHubApi({
   host: "api.github.com", // should be api.github.com for GitHub
   pathPrefix: "", // for some GHEs; none for GitHub
   headers: {
-    "user-agent": "github-es-exporter" // GitHub is happy with a unique user agent
+    "user-agent": "github-es-exporter",
+    "Accept": "application/vnd.github.squirrel-girl-preview",
   },
   Promise: require('bluebird'),
   followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
@@ -28,26 +37,55 @@ github.authenticate({
   token: process.env.GITHUB_TOKEN,
 });
 
+// db = db.connect('data', ['issues', 'comments', 'commits']);
 var queue = new Queue(1, 1000);
 
+function transateToPersistedIssue(gi) {
+  var issue = {
+    number: gi.number,
+    title: gi.title,
+    state: gi.state,
+    comments: gi.comments,
+    labels: _.map(gi.labels, 'name'),
+    milestone: _.get(gi, "milestone.title", null),
+    created_at: gi.created_at,
+    updated_at: gi.updated_at,
+    closed_at: gi.closed_at,
+    user_login: _.get(gi, "user.login", null),
+    assignee: _.get(gi, 'assignee.login', null),
+    is_pull_request: gi.pull_request !== undefined,
+    id: gi.id,
+    reactions_total: gi.reactions.total_count,
+    reactions_plus1: gi.reactions['+1'],
+    reactions_minus1: gi.reactions['-1'],
+    reactions_heart: gi.reactions.heart,
+  };
+  return issue;
+}
+
 function issueListHandler(res) {
-  for (let issue of res.data) {
-    console.log("number", issue.number);
-    console.log("number", issue.number);
+  log.info('got issues', {
+   'rate-limit-remainign': res.meta['x-ratelimit-remaining'],
+    count: res.data.length
+  });
+
+  for (let gi of res.data) {
+    var issue = transateToPersistedIssue(gi);
+    db.saveIssue(issue);
   }
 
   if (github.hasNextPage(res)) {
     setTimeout(() => {
       queue.add(() => {
-        console.log('issueListHandler next page');
+        log.info('github issues getting next page');
         return github.getNextPage(res).then(issueListHandler);
       });
-    }, 1000);
+    }, 5000);
   }
 }
 
-function startIssueWalker() {
-  console.log('issue walker start');
+function startIssueSync() {
+  log.info('issue sync started');
 
   return github.issues.getForRepo({
     owner: "grafana",
@@ -57,7 +95,80 @@ function startIssueWalker() {
   }).then(issueListHandler);
 }
 
-queue.add(startIssueWalker);
+function commentsListHandler(res) {
+  log.info('got comments', {
+   'rate-limit-remaining': res.meta['x-ratelimit-remaining'],
+    count: res.data.length
+  });
+
+  for (let gc of res.data) {
+    let issueNr = gc.issue_url.substr(gc.issue_url.lastIndexOf('/') + 1);
+    var comment = {
+      id: gc.id,
+      issue: issueNr,
+      created_at: gc.created_at,
+      user_login: gc.user.login,
+      reactions_total: gc.reactions.total_count,
+      reactions_plus1: gc.reactions['+1'],
+      reactions_minus1: gc.reactions['-1'],
+      reactions_heart: gc.reactions.heart,
+    }
+    db.saveComment(comment);
+  }
+
+  if (github.hasNextPage(res)) {
+    setTimeout(() => {
+      queue.add(() => {
+        log.info('github comments getting next page');
+        return github.getNextPage(res).then(commentsListHandler);
+      });
+    }, 1000);
+  }
+}
+function startCommentsSync() {
+  log.info('comments sync started');
+
+  let params = {
+    owner: "grafana",
+    repo: "grafana",
+    sort: 'updated',
+    direction: "asc",
+  };
+
+  if (program.sinceDays) {
+    params.since = moment().subtract(parseInt(program.sinceDays), 'days').utc().format();
+  }
+
+  return github.issues.getCommentsForRepo(params).then(commentsListHandler);
+}
+
+program
+  .version('0.0.1')
+  .option('--sinceDays <sinceDays>', 'Date range back in time')
+  .command('issues')
+  .action(function () {
+    queue.add(startIssueSync);
+  });
+
+program
+  .command('comments')
+  .action(function() {
+    queue.add(startCommentsSync);
+  });
+
+program
+  .command('init')
+  .action(function () {
+    db.resetIndex();
+  });
+
+program
+  .command('create-index')
+  .action(function () {
+    db.createIndex();
+  });
+
+program.parse(process.argv);
 
 // console.log("res", res);
 // for (var i = 0; i <= res.length; i++) {
---
+++ db.js
@@ -0,0 +1,92 @@
+
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: 'app'});
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: 'localhost:10200',
+  log: 'error'
+});
+
+function createIndex() {
+  log.info("creating index");
+
+  return client.indices.create({
+    index: "github",
+    body: {
+      "mappings": {
+        "issue": {
+          "properties": {
+            "title":            { "type": "text"  },
+            "state":            { "type": "keyword"  },
+            "labels":           { "type": "keyword"  },
+            "number":           { "type": "keyword"  },
+            "comments":         { "type": "long"  },
+            "assignee":         { "type": "keyword"  },
+            "user_login":       { "type": "keyword"  },
+            "milestone":        { "type": "keyword"  },
+            "created_at":       { "type": "date"  },
+            "closed_at":        { "type": "date"  },
+            "updated_at":       { "type": "date"  },
+            "is_pull_request":  { "type": "boolean"  },
+          }
+        },
+        "comments": {
+          "properties": {
+            "issue":           { "type": "keyword"  },
+            "user_login":      { "type": "keyword"  },
+            "created_at":      { "type": "date"     },
+          }
+        }
+      }
+    }
+  }).then(res => {
+    log.info(res, 'ES index created');
+  }).catch(err => {
+    log.error(err, "index creation failed");
+  });
+}
+
+function resetIndex() {
+  log.info('deleting old ES index');
+  client.indices.delete({index: 'github'})
+    .then(res => {
+      log.info(res, "index deleted");
+      return createIndex();
+    }).catch(err => {
+      log.error("index deletion failed");
+      return createIndex();
+    });
+}
+
+function handleEsError(err, resp) {
+  log.error('ES error', err, resp);
+}
+
+function saveIssue(issue) {
+  return client.index({
+    index: 'github',
+    type: 'issue',
+    id: issue.id,
+    body: issue,
+  }).then(res => {
+    log.info('saved issue to ES', {number: issue.number});
+  }).catch(handleEsError);
+}
+
+function saveComment(comment) {
+  return client.index({
+    index: 'github',
+    type: 'comment',
+    id: comment.id,
+    body: comment,
+  }).then(res => {
+    log.info('saved comment to ES', {created_at: comment.created_at, by: comment.user_login});
+  }).catch(handleEsError);
+}
+
+module.exports = {
+  saveIssue: saveIssue,
+  saveComment: saveComment,
+  resetIndex: resetIndex,
+  createIndex: createIndex,
+};
